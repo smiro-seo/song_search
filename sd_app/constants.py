ece752200cd6fd9ed891fa3508049ec87f5cc433
faucac <faustocaceres1@gmail.com>
2022-12-27 13:21:02 +0100

    keys

--- sd_app/constants.py
+++ sd_app/constants.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 
 keys={}
-with open(os.path.join(__file__,'keys.txt'), 'r') as f:
+with open(os.path.join(Path(__file__).parent.absolute(),'keys.txt'), 'r') as f:
     key_list = f.readlines()
 keys['openai_key'] = key_list[0]
 keys['youtube_key'] = key_list[1]
