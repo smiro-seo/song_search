eaf6ab19a90489926aba1f01a0d7faed28aa07a6
faucac <faustocaceres1@gmail.com>
2022-12-27 13:19:15 +0100

    correction

--- sd_app/constants.py
+++ sd_app/constants.py
@@ -1,7 +1,7 @@
 import os
 
 keys={}
-with open(os.path.join(os.getcwd(),'keys.txt'), 'r') as f:
+with open(os.path.join(__file__,'keys.txt'), 'r') as f:
     key_list = f.readlines()
 keys['openai_key'] = key_list[0]
 keys['youtube_key'] = key_list[1]
