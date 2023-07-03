14306a3bcadd658b277d47176176c5079bb56988
faucac <faustocaceres1@gmail.com>
2023-07-03 14:06:12 -0300

    rename pics

--- sd_app/static/feat_images/rename_pictures.py
+++ sd_app/static/feat_images/rename_pictures.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 import pandas as pd
 import json
+import numpy as np
 
 def clean_name(name):
     new_name = name.replace(' ', '_').replace('-', '_').replace('(', '').replace(')', '').replace('.', '').replace(',', '')
@@ -13,14 +14,17 @@ cur = conn.cursor()
 
 searches = pd.read_sql_query("SELECT id, keywords, by FROM search", conn)
 
+print("Files: " + str(os.listdir()))
+
 for pic_name in os.listdir():
     
     if 'feat_img' in pic_name:
-        search_id = pic_name.split('_')[-1].split('.')[0]
+        search_id = np.int64(pic_name.split('_')[-1].split('.')[0])
 
-        if search_id in searches['id'].values:
-            
-            search = searches[searches.id==search_id][0]
+        if search_id in list(searches['id'].values):
+
+            search = searches[searches['id']==search_id].iloc[0]
+            print(search)
 
             if search['by']=='artist':
                 kw = search['keywords']
@@ -29,5 +33,5 @@ for pic_name in os.listdir():
             
             kw = clean_name(kw)
 
-            new_name = f'{kw}_{search_id}.png'
+            new_name = f'{kw}_songs_{search_id}.png'
             os.rename(pic_name, new_name)
