7352192575dbb1b7023c05a5ddc81943a47f1258
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-05-14 11:58:59 +0300

    req

--- search/ai.py
+++ search/ai.py
@@ -9,6 +9,7 @@ import io, base64
 import requests
 local=False
 
+
 cwd = os.path.dirname(__file__)
 output_dir = os.path.join(cwd, '..', '..', '..', '..', 'var', 'song_search', 'feat_images') if not local else os.path.join(cwd,'feat_images')
 
